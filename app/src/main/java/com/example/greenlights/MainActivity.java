f9b27f25d2d1a36fce3d923eb398e14a3ac886a6
slayedcodes <completedcae@gmail.com>
2022-04-10 20:13:16 +0300

    Added Basic Map Functions, connected to firebase

--- app/src/main/java/com/example/greenlights/MainActivity.java
+++ app/src/main/java/com/example/greenlights/MainActivity.java
@@ -2,10 +2,18 @@ package com.example.greenlights;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,21 +21,62 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    static LatLng LatLngSubmit;
+    static String Location;
+    GoogleMap mMap;
+    String location;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-            // Get the SupportMapFragment and request notification when the map is ready to be used.
-            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                    .findFragmentById(R.id.mapView1);
+        SearchView searchView = findViewById(R.id.editSearchText);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mapView1);
         if (mapFragment != null) {
             mapFragment.getMapAsync(this);
         }
-    }
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                location = searchView.getQuery().toString();
+                List<Address> addressList = null;
+
+                if (location!=null || !location.equals("")){
+                    Geocoder geocoder = new Geocoder(MainActivity.this);
+                    try{
+                        addressList = geocoder.getFromLocationName(location,1);
+                    } catch (IOException e) {
+                        Toast.makeText(MainActivity.this,"Location not found", Toast.LENGTH_SHORT).show();
+                    }
+
+                    Address address = addressList.get(0);
+                    LatLng latLng = new LatLng(address.getLatitude(),address.getLongitude());
+                    LatLngSubmit = latLng;
+                    mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+        }
 
         /**
          * Manipulates the map when it's available.
@@ -40,13 +89,45 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
          */
         @Override
         public void onMapReady(GoogleMap googleMap) {
+            mMap = googleMap;
             // Add a marker in Sydney, Australia,
             // and move the map's camera to the same location.
             LatLng sydney = new LatLng(-33.852, 151.211);
-            googleMap.addMarker(new MarkerOptions()
+            mMap.addMarker(new MarkerOptions()
                     .position(sydney)
                     .title("Marker in Sydney"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+            mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+                @Override
+                public void onMapLongClick(LatLng latLng) {
+                    mMap.clear();
+                    mMap.addMarker(new MarkerOptions().position(latLng));
+                    Dialog d = new Dialog(MainActivity.this);
+                    d.setContentView(R.layout.activity_dialog_add_crash);
+                    d.show();
+
+                    EditText editLat = d.findViewById(R.id.editTextLat);
+                    editLat.setEnabled(false);
+
+                    EditText editLong = d.findViewById(R.id.editTextLong);
+                    editLong.setEnabled(false);
+
+                    editLat.setText(Double.toString(latLng.latitude));
+                    editLong.setText(Double.toString(latLng.longitude));
+
+
+
+                }
+            });
+
+            mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                @Override
+                public void onMapClick(LatLng latLng) {
+                    mMap.clear();
+                    mMap.addMarker(new MarkerOptions().position(latLng));
+                }
+            });
         }
     }
 
